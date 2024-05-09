692221dc17157a51830d924bd4825dbae874417d
Eric Roberson <109642366+Erob711@users.noreply.github.com>
2024-05-09 16:43:06 -0700

    art card and header components (#2)
    * art  card and header
    
    * remove random func

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import metMuseumService from './services/metMuseum'
+import { Header } from './components/Header'
+import { ArtCard } from './components/ArtCard'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [art, setArt] = useState({})
 
   useEffect(() => {
     // const fetchData = async () => {
@@ -30,31 +33,28 @@ function App() {
     const fetchData = async () => {
       return await metMuseumService.fetchDataBySearchQuery('sunflowers');
     }
+    
     console.log(fetchData());
   }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Header />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={async () => {
+          await metMuseumService.
+            fetchDataById(Math.floor(Math.random() * 100) + 1)
+            .then((data) => setArt(data || {}));
+        }}>
+          More Art
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/* maybe decide to pass art data w/ context? */}
+      <ArtCard art={art} />
     </>
   )
 }
