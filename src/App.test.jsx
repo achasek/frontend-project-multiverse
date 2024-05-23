2f6086be1a9274527df5d1ca01bd8d3a4bd99eff
Chase AK <109045340+achasek@users.noreply.github.com>
2024-05-23 14:10:41 -0700

    (Tests): Added more tests to existing suite (#9)

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App.jsx', () => {
     render(<App />);
 
-    const elementToView = screen.getByText('More Art');
-
     it('renders the home page', () => {
+        const elementToView = screen.queryByText('More Art');
+
         expect(elementToView).toBeDefined();
+    })
+
+    it('has initial data populated in the <ArtCard />', () => {
+        const elementToView = screen.queryByText('Two-and-a-Half Dollar Coin');
+
+        expect(elementToView).toBeDefined();
+    })
+
+    it('replaces the initial data on button click', async () => {
+        const elementToView = screen.queryByText('Two-and-a-Half Dollar Coin');
+
+        const user = userEvent.setup();
+
+        const button = screen.queryByText('More Art');
+
+        await user.click(button);
+
+        expect(elementToView).toBeNull();
     })
 });
